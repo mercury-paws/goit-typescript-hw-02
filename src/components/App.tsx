a912379f0d87e664a2af863a4f54ad62183bb3a0
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-06-03 11:14:52 +0200

    ts2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,11 +6,24 @@ import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 import SearchBar from "./SearchBar/SearchBar";
 import { fetchImages } from "../data-api";
 import { useEffect, useState } from "react";
-
+interface Items {
+  id: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  alt_description: string;
+  description: string;
+  user: {
+    bio: string;
+    location: string;
+    total_photos: number;
+  };
+}
 export default function App() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
-  const [photos, setPhotos] = useState<Array<any>>([]);
+  const [photos, setPhotos] = useState<Array<Items>>([]);
   const [page, setPage] = useState<number>(1);
   const [query, setQuery] = useState<string>("");
   const [modalOpen, setModalOpen] = useState<boolean>(false);
--- src/data-api.ts
+++ src/data-api.ts
@@ -1,13 +1,28 @@
 import axios, { AxiosResponse } from "axios";
+import { RefObject } from "react";
 
 const API_KEY: string = "IBl9vq3xGxkJ6VtwLlkDtCBI-G8aE2lghdIxtwr96cY";
 
 axios.defaults.baseURL = "https://api.unsplash.com/search/";
 
+interface Data {
+  id: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  alt_description: string;
+  description: string;
+  user: {
+    bio: string;
+    location: string;
+    total_photos: number;
+  };
+}
 export const fetchImages = async (
   searchQuery: string,
   currentPage: number
-): Promise<any> => {
+): Promise<Data[]> => {
   const response: AxiosResponse = await axios.get("photos/", {
     params: {
       query: searchQuery,
